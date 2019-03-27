e3da7e2be0bc67af1c3b5d7463a6b6c00f2690b6
excander <excander@gmail.com>
2019-03-27 22:44:53 +0300

    добавил const'ы, изменил int64_t, убрал глобальные перем, убрал using namespace std...

--- hw/02/calc.cpp
+++ hw/02/calc.cpp
@@ -1,104 +1,100 @@
-#include<iostream>
-#include<exception>
+#include <iostream>
+#include <exception>
 
-using namespace std;
-
-char getlex(string &str);
-int expr(string &str, char &curlex);
-int add(string &str, char &curlex);
-int mult(string &str, char &curlex);
+char getlex(const std::string &str);
+int64_t expr(const std::string &str, char &curr);
+int64_t add(const std::string &str, char &curr);
+int64_t mult(const std::string &str, char &curr);
 size_t ind = 0;
 
-char my_getchar(string &str){
+char my_getchar(const std::string &str){
     if (ind < str.size())
         return str[ind++];
-    else {
-        return '\n';
-    }
+    else return '\n';
 }
 
-char getlex(string &str){
-    char curlex;
-    while ( (curlex = my_getchar(str)) == ' ' );
-    return curlex;
+char getlex(const std::string &str){
+    char curr;
+    while ((curr = my_getchar(str)) == ' ');
+    return curr;
 }
 
-int expr(string &str, char &curlex){
-    int e = add(str, curlex);
-    while (curlex == '+' || curlex == '-') {
-        if (curlex == '+') {
-            curlex = getlex(str);
-            e += add(str, curlex);
+int64_t expr(const std::string &str, char &curr){
+    int64_t e = add(str, curr);
+    while (curr == '+' || curr == '-') {
+        if (curr == '+') {
+            curr = getlex(str);
+            e += add(str, curr);
         }
-        else if (curlex == '-') {
-            curlex = getlex(str);
-            e -= add(str, curlex);
+        else if (curr == '-') {
+            curr = getlex(str);
+            e -= add(str, curr);
         }
     }
     return e;
 }
 
-int add(string &str, char &curlex){
-    int a=mult(str, curlex);
-    while (curlex == '*' || curlex == '/') {
-        if (curlex == '*'){
-            curlex = getlex(str);
-            a *= mult(str, curlex);
+int64_t add(const std::string &str, char &curr){
+    int64_t a = mult(str, curr);
+    while (curr == '*' || curr == '/') {
+        if (curr == '*'){
+            curr = getlex(str);
+            a *= mult(str, curr);
         }
-        else if (curlex == '/') {
-            curlex = getlex(str);
-            int t=mult(str, curlex);
+        else if (curr == '/') {
+            curr = getlex(str);
+            int64_t t = mult(str, curr);
             if (t)
                 a /= t;
             else{
-                throw exception();
+                throw std::exception();
             }
         }
     }
     return a;
 }
 
-int mult(string &str, char &curlex){
-    int m;
-    switch(curlex){
+int64_t mult(const std::string &str, char &curr){
+    int64_t m;
+    switch(curr){
         case '0': case '1': case '2': case '3': case '4': case '5':
         case '6': case '7': case '8': case '9':{
-            m= curlex-'0';
+            m = curr - '0';
             break;
         }
-        case '(': curlex = getlex(str); m=expr(str, curlex);
-            if (curlex == ')'){
+        case '(': curr = getlex(str); m = expr(str, curr);
+            if (curr == ')'){
                 break;
             }
-        case '-': curlex = getlex(str); m=-1*mult(str, curlex);
+        case '-': curr = getlex(str); m = -1 * mult(str, curr);
                 break;
         default : {
-            throw exception();
+            throw std::exception();
         }
     }
-    if (curlex!='\n' && curlex!='/'&& curlex!='*' && curlex!='+' && curlex!='-')
-        curlex = getlex(str);
+    if (curr != '\n' && curr != '/' && curr != '*' && curr != '+' && curr != '-')
+        curr = getlex(str);
     return m;
 }
 
 int main(int argc, char* argv[]){
 
-    int res;
-    string str;
-    char curlex;
+    int64_t res;
+    std::string str;
+    char curr;
 
     try{
         if(argc != 2)
-            throw exception();
+            throw std::exception();
         else {
         str = argv[1];
-            curlex = getlex(str);
-            res=expr(str, curlex);
-            cout << res << endl;
+            curr = getlex(str);
+            res = expr(str, curr);
+            std::cout << res << std::endl;
         }
     }
     catch (std::exception){
-        cout << "error" << endl;
+        std::cout << "error" << std::endl;
         return 1;
     }
 
