f3f5c86d156edce8bac4eac07ee73760f2995de6
excander <excander@gmail.com>
2019-03-27 21:53:21 +0300

    пройдены тесты

--- hw/02/calc.cpp
+++ hw/02/calc.cpp
@@ -8,7 +8,6 @@ void getlex(void);
 int expr(void);
 int add(void);
 int mult(void);
-void error();
 string str;
 size_t ind = 0;
 
@@ -16,7 +15,6 @@ char my_getchar(){
 	if (ind < str.size())
 		return str[ind++];
 	else {
-		// cout << "mY_getchar_return_"<< endl;
 		return '\n';
 	}
 }
@@ -25,16 +23,8 @@ void getlex(){
     while ( (curlex = my_getchar()) == ' ' );
 }
 
-void error(void){
-    printf("\nERROR!\n");
-    while(my_getchar()!='\n');
-    // longjmp(begin,1);
-
-}
-
 int expr(){
     int e = add();
-    // cout << "curlex_add = " << curlex << endl;
     while (curlex == '+' || curlex == '-') {
         if (curlex == '+') {
             getlex();
@@ -61,8 +51,7 @@ int add(){
             if (t)
                 a /= t;
             else{
-            	cout<< "error 1";
-                error();
+            	throw exception();
             }
         }
     }
@@ -74,23 +63,17 @@ int mult(){
     switch(curlex){
         case '0': case '1': case '2': case '3': case '4': case '5':
         case '6': case '7': case '8': case '9':{
-        	// cout << "yep";
             m= curlex-'0';
-            // m = trypow(m);
             break;
         }
         case '(': getlex(); m=expr();
             if (curlex == ')'){
-                // m = trypow(m);
                 break;
             }
         case '-': getlex(); m=-1*mult();
      			break;
         default : {
-        	cout << "curlex = " << curlex << endl;
-        	// cout << str[ind+1] << endl;
-        	cout<< "error 3";
-         	error();
+        	throw exception();
         }
     }
     if (curlex!='\n' && curlex!='/'&& curlex!='*' && curlex!='+' && curlex!='-')
@@ -107,14 +90,13 @@ int main(int argc, char* argv[]){
             throw exception();
         else {
        	str = argv[1];
-            // str = "2+2";
         	getlex();
     		res=expr();
         	cout << res << endl;
         }
     }
     catch (std::exception){
-        cout << "Error!" << endl;
+        cout << "error" << endl;
         return 1;
     }
 
